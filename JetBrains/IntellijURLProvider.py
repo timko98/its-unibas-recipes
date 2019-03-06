19e7edbb665e554024d93ee5f5eb735cab29f766
Tom Cinbis <tom.cinbis@unibas.ch>
2019-03-06 11:20:17 +0100

    Updated Jetbrains Url Providers to handle new version XML file
    This closes issue #3

--- JetBrains/IntellijURLProvider.py
+++ JetBrains/IntellijURLProvider.py
@@ -9,10 +9,9 @@
 #
 
 import urllib2
-import xml.etree.cElementTree as ET
+from xml.dom import minidom
 
 from autopkglib import Processor, ProcessorError
-
 
 __all__ = ["IntellijURLProvider"]
 
@@ -20,62 +19,82 @@ intellij_version_url = 'https://www.jetbrains.com/updates/updates.xml'
 
 
 class IntellijURLProvider(Processor):
-  """Provide URL for latest Intellij IDEA build."""
+    """Provide URL for latest Intellij IDEA build."""
 
-  description = "Provides URL and version for the latest release of Intellij."
-  input_variables = {
-    "base_url": {
-      "required": False,
-      "description": ('Default is '
-                      'https://www.jetbrains.com/updates/updates.xml'),
-    },
-    "edition": {
-      "required": False,
-      "description": ('Either "C" for "Community" or "U" for "Ultimate" '
-                      'edition. Defaults to "C".')
+    description = "Provides URL and version for the latest release of Intellij."
+    input_variables = {
+        "base_url": {
+            "required": False,
+            "description": ('Default is '
+                            'https://www.jetbrains.com/updates/updates.xml'),
+        },
+        "edition": {
+            "required": False,
+            "description": ('Either "C" for "Community" or "U" for "Ultimate" '
+                            'edition. Defaults to "C".')
+        }
     }
-  }
-  output_variables = {
-    "url": {
-      "description": "URL to the latest release of Intellij",
+    output_variables = {
+        "url": {
+            "description": "URL to the latest release of Intellij",
+        }
     }
-  }
 
-  __doc__ = description
+    __doc__ = description
 
-  def get_intellij_version(self, intellij_version_url):
-    """Retrieve version number from XML."""
-    # Read XML
-    try:
-        req = urllib2.Request(intellij_version_url)
-        f = urllib2.urlopen(req)
-        html = f.read()
-        f.close()
-    except BaseException as e:
-        raise ProcessorError(
-          "Can't download %s: %s" % (
-            intellij_version_url, e)
+    def get_intellij_version(self, intellij_version_url):
+        """Retrieve version number from XML."""
+        # Read XML
+        try:
+            req = urllib2.Request(intellij_version_url)
+            f = urllib2.urlopen(req)
+            html = f.read()
+            f.close()
+        except BaseException as e:
+            raise ProcessorError(
+                'Can not download %s: %s' % (
+                    intellij_version_url, e)
+            )
+
+        root = minidom.parseString(html)
+        # Get all products in the XML
+        products = root.childNodes[0].getElementsByTagName('product')
+
+        intellij_product = None
+        for product in products:
+            if product.hasAttribute('name') and product.getAttribute('name') == 'IntelliJ IDEA':
+                intellij_product = product
+
+        if intellij_product is not None:
+            channels = intellij_product.getElementsByTagName('channel')
+            for channel in channels:
+                if channel.hasAttribute('licensing') and channel.getAttribute('licensing') == 'release':
+                    builds = channel.getElementsByTagName('build')
+                    available_versions = list()
+                    for build in builds:
+                        if build.hasAttribute('version'):
+                            available_versions.append(build.getAttribute('version'))
+                    available_versions.sort(reverse=True)
+                    # We can return here because we found the release channel.
+                    return str(available_versions[0])
+        else:
+            raise ProcessorError(
+                'Did not find Intellij in version XML.'
+            )
+
+    def main(self):
+        """Main function."""
+        # Determine values.
+        version_url = self.env.get('version_url', intellij_version_url)
+        version = self.get_intellij_version(version_url)
+        download_url = (
+                'https://download.jetbrains.com/idea/'
+                'ideaI%s-%s.dmg' % (self.env.get('edition', 'C'), version)
         )
-    # Search for download link.
-    root = ET.fromstring(html)
-    # root[0][-1] is always the last IDEA release
-    build = root[0][-1].find('build')
-    version = build.attrib['version']
-    # Return pkg url.
-    return str(version)
 
-  def main(self):
-    """Main function."""
-    # Determine values.
-    version_url = self.env.get('version_url', intellij_version_url)
-    version = self.get_intellij_version(version_url)
-    download_url = (
-      "https://download.jetbrains.com/idea/"
-      "ideaI%s-%s.dmg" % (self.env.get('edition', 'C'), version)
-    )
+        self.env['url'] = download_url
+        self.output('URL: %s' % self.env['url'])
 
-    self.env["url"] = download_url
-    self.output("URL: %s" % self.env["url"])
 
 if __name__ == '__main__':
     processor = IntellijURLProvider()
