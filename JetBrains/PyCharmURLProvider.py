2f8f024ec5cf7afdc6ec931fd9aecc8e7f0bba3e
Tim Keller <tim.keller@unibas.ch>
2020-01-30 14:45:28 +0100

    Add for the PyCharmUrlProvider

--- JetBrains/PyCharmURLProvider.py
+++ JetBrains/PyCharmURLProvider.py
@@ -15,10 +15,15 @@ from xml.dom import minidom
 
 from autopkglib import Processor, ProcessorError
 
+import sys
+
 try:
-    from urllib.request import urlopen  # For Python 3
+    # For Python 3
+    from urllib.request import urlopen
+    import certifi
 except ImportError:
-    from urllib2 import urlopen  # For Python 2
+    # For Python 2
+    from urllib2 import urlopen
 
 __all__ = ["PyCharmURLProvider"]
 
@@ -52,9 +57,15 @@ class PyCharmURLProvider(Processor):
         """Retrieve version number from XML."""
         # Read XML
         try:
-            f = urlopen(intellij_version_url)
-            html = f.read()
-            f.close()
+            if sys.version_info.major < 3:
+                f = urlopen(intellij_version_url)
+                html = f.read()
+                f.close()
+            else:
+                f = urlopen(intellij_version_url, cafile=certifi.where())
+                html = f.read().decode("utf-8")
+                f.close()
+            
         except Exception as e:
             raise ProcessorError("Can not download %s: %s" % (intellij_version_url, e))
 
